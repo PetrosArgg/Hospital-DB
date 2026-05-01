1c090af397aca0f807f7d0b90e8744986bbe042b
PetrosArgiris <petros.argiris@sigfox.gr>
2026-05-01 18:40:07 +0300

    first commit!

---
+++ sql/install.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STAFF (
+  amka CHAR(11) PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  birth_date DATE,
+  email VARCHAR(100),
+  phone VARCHAR(15),
+  hire_date DATE
+)
