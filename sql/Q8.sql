3c650ceaf4267a21cf9e701cf7e84b29a58c9be7
PetrosArgiris <petros.argiris@sigfox.gr>
2026-05-13 13:42:38 +0300

    addded query page for ui

--- sql/Q8.sql
+++ sql/Q8.sql
@@ -5,14 +5,13 @@ SELECT
     s.last_name,
     s.staff_type,
     s.birth_date,
-    TIMESTAMP(YEAR, s.birth_date, CURDATE()) AS age,
+    TIMESTAMPDIFF(YEAR, s.birth_date, CURDATE()) AS age,
     s.email,
     s.phone,
     s.hire_date,
-    s.staff_type,
     d.license_number,
     d.specialty,
-    d.rank AS doctor_rank
+    d.rank AS doctor_rank,
     d.supervisor_id,
     n.rank AS nurse_rank,
     a.duty_role,
@@ -34,17 +33,16 @@ FROM Staff s
 LEFT JOIN Doctors d ON s.id = d.staff_id
 LEFT JOIN Nurses n ON s.id = n.staff_id
 LEFT JOIN Administrative_Staff a ON s.id = a.staff_id
-JOIN Departments dep ON dep.name = '...'
+JOIN Departments dep ON dep.name = 'Χειρουργική'
 WHERE s.active = TRUE
     AND (EXISTS (SELECT 1 FROM Doctor_Departments dd
                     WHERE dd.doctor_id = s.id AND dd.department_id = dep.id))
-         OR (n.department_id = dep.id)
-         OR (a.department_id = dep.id)
-         )
+         OR n.department_id = dep.id
+         OR a.department_id = dep.id
     AND s.id NOT IN (
         SELECT ss.staff_id
         FROM Staff_Shifts ss
-        JOIN Shifts sh ON ss.shifts_id = sh.id
-        WHERE sh.shift_date = '2026-05-1' AND sh.department_name = dep.id
+        JOIN Shifts sh ON ss.shift_id = sh.id
+        WHERE sh.shift_date = '2026-05-1' AND sh.department_id = dep.id
     )
 ORDER BY s.staff_type, s.last_name, s.first_name;
