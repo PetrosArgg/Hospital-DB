cab5a1445aaae0516cd0a50a47de20cd83be2c22
PetrosArgiris <petros.argiris@sigfox.gr>
2026-05-18 22:01:21 +0300

    changed wrong output names addded output for q4 and 6 and fixed broken index in q4

--- sql/Q04a.sql
+++ sql/Q04a.sql
@@ -1,11 +1,14 @@
-EXPLAIN ANALYZE
-SELECT 
+-- Τα αποτελέσματα των ANALYZE FORMAT=JSON και η σύγκριση 
+-- κόστους/χρόνου βρίσκονται αναλυτικά στο docs/report.pdf
+-- ANALYZE FORMAT=JSON
+
+SELECT
     s.last_name AS 'Επώνυμο Ιατρού',
     s.first_name AS 'Όνομα Ιατρού',
     AVG(dr.medical_care_quality) AS 'Μέση Ποιότητα Ιατρικής Φροντίδας',
     AVG(hr.overall_experience) AS 'Μέση Συνολική Εντύπωση Νοσηλείας'
 FROM Doctor_Ratings dr
-JOIN Hospitalization_Ratings hr ON dr.hospitalization_id = hr.hospitalization_id
+LEFT JOIN Hospitalization_Ratings hr ON dr.hospitalization_id = hr.hospitalization_id
 JOIN Staff s ON dr.doctor_id = s.id
-WHERE dr.doctor_id = 1 
+WHERE dr.doctor_id = 2
 GROUP BY dr.doctor_id, s.last_name, s.first_name;
--- sql/Q04b.sql
+++ sql/Q04b.sql
@@ -1,11 +1,13 @@
-EXPLAIN ANALYZE
+-- Τα αποτελέσματα των ANALYZE FORMAT=JSON και η σύγκριση 
+-- κόστους/χρόνου βρίσκονται αναλυτικά στο docs/report.pdf
+-- ANALYZE FORMAT=JSON
 SELECT 
     s.last_name AS 'Επώνυμο Ιατρού',
     s.first_name AS 'Όνομα Ιατρού',
     AVG(dr.medical_care_quality) AS 'Μέση Ποιότητα Ιατρικής Φροντίδας',
     AVG(hr.overall_experience) AS 'Μέση Συνολική Εντύπωση Νοσηλείας'
-FROM Doctor_Ratings dr FORCE INDEX (idx_docrat_doc)
-JOIN Hospitalization_Ratings hr FORCE INDEX (idx_hosprat_hosp) ON dr.hospitalization_id = hr.hospitalization_id
+FROM Doctor_Ratings dr FORCE INDEX (idx_docrat_doctor)
+LEFT JOIN Hospitalization_Ratings hr FORCE INDEX (idx_hosprat_hosp) ON dr.hospitalization_id = hr.hospitalization_id
 JOIN Staff s FORCE INDEX (PRIMARY) ON dr.doctor_id = s.id
-WHERE dr.doctor_id = 1 
+WHERE dr.doctor_id = 2 
 GROUP BY dr.doctor_id, s.last_name, s.first_name;
